a77578c4586a9e36f12da809569cfe3e32611242
Ansh-Srivastava-1425 <kumar.ansh7591@gmail.com>
2026-06-03 12:47:19 +0530

    backend 2

--- backend2/src/app.js
+++ backend2/src/app.js
@@ -17,11 +17,37 @@ app.post("/notes", (req, res) => {
   });
 });
 
+
+//GET METHOD DATA KO BACKEND SE FRONTEND P LATA H
 app.get("/notes", (req, res) => {
   res.status(200).json({
     message: "Message fetched successfully",
     notes: notes,
   });
 });
+
+//DELETE METHOD PRESENT DATA KO DELETE KRTA H
+app.delete("/notes/:index" , (req ,res) => {
+  const index = req.params.index;
+
+  delete notes[index];
+
+  res.status(200).json({
+    message: "Note deleted successfully",
+  })
+})
+
+//PATCH METHOD DATA KO PARTIALY UPDATE KRNE KE LIYE USE HOTA H
+app.patch("/notes/:index" , (req , res) =>{
+  const index = req.params.index;
+
+  const description = req.body.description;
+
+  notes[index].description = description;
+
+  res.status(200).json({
+    message: "Note updated successfully",
+  })
+})
 
 module.exports = app;
