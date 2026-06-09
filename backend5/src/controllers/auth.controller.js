0560949faaf99bab05d5ab2f824e326bcee08850
Ansh-Srivastava-1425 <kumar.ansh7591@gmail.com>
2026-06-09 23:58:19 +0530

    auth

--- backend5/src/controllers/auth.controller.js
+++ backend5/src/controllers/auth.controller.js
@@ -2,22 +2,29 @@ const userModel = require("../models/user.model");
 const jwt = require('jsonwebtoken')
 
 async function registerUser(req, res) {
-  const { userName, email, password } = req.body;
+  const {userName , email , password} = req.body;
+
+  const userAlreadyExists = await userModel.findOne({email})
+
+  if(userAlreadyExists){
+    return res.status(409).json({
+      message:"user Already "
+    })
+  }
 
   const user = await userModel.create({
-    userName,
-    email,
-    password,
-  });
+    userName , email , password
+  })
 
   const token = jwt.sign({
-    id:user._id
-  } , process.env.JWT_SECRET)
+    id : user._id , 
+  }, process.env.JWT_SECRET)  
+
+  res.cookie("token" , token)
 
   res.status(201).json({
-    message:"user resgisterd",
-    user,
-    token
+    message:"User registered successfully",
+    user
   })
 }
 
