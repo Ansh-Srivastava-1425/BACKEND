e62965675295798b37c81b7aee7ca81a1ffec902
Ansh-Srivastava-1425 <kumar.ansh7591@gmail.com>
2026-06-11 19:54:47 +0530

    spotify

---
+++ project2/backend6/controllers/music.controller.js
@@ -0,0 +1,2 @@
+const musicModel = require("../models/music.model")
+
--- project2/backend6/models/user.model.js
+++ project2/backend6/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     role:{
         type : String,
-        enum : ['admin' , 'user'],
+        enum : ['artist' , 'user'],
         default : 'user'
     }
 })
